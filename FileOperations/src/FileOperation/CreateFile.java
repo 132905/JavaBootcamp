e2e818bf3de86d7f5fc0f534e8733d3eabeb08cb
132905 <125987008+132905@users.noreply.github.com>
2024-02-19 22:52:42 +0530

    File Operations

---
+++ FileOperations/src/FileOperation/CreateFile.java
@@ -0,0 +1,24 @@
+package FileOperation;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class CreateFile {
+
+	public static void main(String[] args) {
+
+		Path filePath = Paths.get("C:\\Users\\Harsh\\Desktop\\FileOperationsWorkspace\\newfile.txt");
+
+		try {
+			// Creating the new file
+			Files.createFile(filePath);
+
+			System.out.println("File created successfully!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ FileOperations/src/FileOperation/FileExits.java
@@ -0,0 +1,19 @@
+package FileOperation;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileExits {
+
+	public static void main(String[] args) {
+
+		Path filePath = Paths.get("C:\\Users\\Harsh\\Desktop\\FileOperationsWorkspace\\newfile.txt");
+
+		// Check if the file exists
+		boolean fileExists = Files.exists(filePath);
+
+		System.out.println("File exists: " + fileExists);
+	}
+
+}
